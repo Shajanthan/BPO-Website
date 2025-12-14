23fb8b7ae9adf018d4794387dda0e93d875ef91b
Shajanthan <jshajanthan@gmail.com>
2025-12-14 19:33:36 +0530

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-import ScrollToTop from './components/ScrollToTop'
-import WhatsAppFloat from './components/WhatsAppFloat'
-import Home from './pages/Home'
-import Services from './pages/Services'
-import About from './pages/About'
-import Contact from './pages/Contact'
-import Resources from './pages/Resources'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import ScrollToTop from "./components/ScrollToTop";
+import WhatsAppFloat from "./components/WhatsAppFloat";
+import Home from "./pages/Home";
+import Services from "./pages/Services";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Resources from "./pages/Resources";
 
 function App() {
   return (
@@ -28,8 +28,7 @@ function App() {
         <WhatsAppFloat />
       </div>
     </Router>
-  )
+  );
 }
 
-export default App
-
+export default App;
